2be02712c00a606a5b4104a3490d8972a14d0f88
sinku <sininoir@gmail.com>
2022-07-18 13:58:35 +0430

    Add base main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from sys import argv
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+from GithubUser import GithubUser
+
+if len(argv) < 2:
+    print("Usage: main.py <username>")
+    exit(1)
+
+username = argv[1]
+
+github_user = GithubUser(username)
+github_user.print_following_followers_diff()
